ae5e5c945bac047e853ff3509ab8054b87db1e34
Silohi Andreea <andreea.silohi@bearingpoint.com>
2024-03-27 22:31:04 +0200

    Delete order from admin view

--- frontend/src/OrderListScreen/OrderListScreen.js
+++ frontend/src/OrderListScreen/OrderListScreen.js
@@ -4,7 +4,7 @@ import Navbar from "../navbar/Navbar";
 import LoadingBox from "../LoadingBox";
 import MessageBox from "../MessageBox";
 import { Store } from "../Store";
-import axios from 'axios';
+import axios from "axios";
 import {
   Button,
   Table,
@@ -15,7 +15,7 @@ import {
   TableRow,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import{ useContext, useEffect, useReducer } from "react";
+import { useContext, useEffect, useReducer } from "react";
 const reducer = (state, action) => {
   switch (action.type) {
     case "FETCH_REQUEST":
@@ -28,6 +28,21 @@ const reducer = (state, action) => {
       };
     case "FETCH_FAIL":
       return { ...state, loading: false, error: action.payload };
+
+    case "DELETE_REQUEST":
+      return { ...state, loadingDelete: true, successDelete: false };
+    case "DELETE_SUCCESS":
+      return {
+        ...state,
+        loadingDelete: false,
+        successDelete: true,
+      };
+    case "DELETE_FAIL":
+      return { ...state, loadingDelete: false };
+
+    case "DELETE_RESET":
+      return { ...state, loadingDelete: false, successDelete: false };
+
     default:
       return state;
   }
@@ -37,10 +52,11 @@ export default function OrderListScreen() {
   const { state } = useContext(Store);
   const { userInfo } = state;
 
-  const [{ loading, error, orders }, dispatch] = useReducer(reducer, {
-    loading: true,
-    error: "",
-  });
+  const [{ loading, error, orders, loadingDelete, successDelete }, dispatch] =
+    useReducer(reducer, {
+      loading: true,
+      error: "",
+    });
 
   useEffect(() => {
     const fetchData = async () => {
@@ -57,9 +73,30 @@ export default function OrderListScreen() {
         });
       }
     };
-    fetchData();
-  }, [userInfo]);
+    if (successDelete) {
+      dispatch({ type: "DELETE_RESET" });
+    } else {
+      fetchData();
+    }
+  }, [userInfo, successDelete]);
 
+  const deleteHandler = async (order) => {
+    if (window.confirm("Are you sure to delete?")) {
+      try {
+        dispatch({ type: "DELETE_REQUEST" });
+        await axios.delete(`/api/orders/${order._id}`, {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        });
+        window.alert("Order deleted successfully");
+        dispatch({ type: "DELETE_SUCCESS" });
+      } catch (err) {
+        window.alert(getError(error));
+        dispatch({
+          type: "DELETE_FAIL",
+        });
+      }
+    }
+  };
   return (
     <div className="container-order">
       <div className="navbar-place-order">
@@ -67,7 +104,7 @@ export default function OrderListScreen() {
       </div>
       <div className="order-history-content">
         <h1>Orders</h1>
-
+        {loadingDelete && <LoadingBox></LoadingBox>}
         {loading ? (
           <LoadingBox></LoadingBox>
         ) : error ? (
@@ -96,13 +133,9 @@ export default function OrderListScreen() {
                     </TableCell>
                     <TableCell>{order.createdAt}</TableCell>
                     <TableCell>{order.totalPrice}</TableCell>
+                    <TableCell>{order.isPaid ? order.paidAt : "No"}</TableCell>
                     <TableCell>
-                      {order.isPaid ? order.paidAt : "No"}
-                    </TableCell>
-                    <TableCell>
-                      {order.isDelivered
-                        ? order.deliveredAt
-                        : "No"}
+                      {order.isDelivered ? order.deliveredAt : "No"}
                     </TableCell>
                     <TableCell>
                       <Button
@@ -112,18 +145,21 @@ export default function OrderListScreen() {
                       >
                         Details
                       </Button>
+                      &nbsp;
+                      <Button
+                        type="button"
+                        variant="light"
+                        onClick={() => deleteHandler(order)}
+                      >
+                        Delete
+                      </Button>
                     </TableCell>
-
-          
                   </TableRow>
                 ))}
               </TableBody>
             </Table>
-
-            
           </TableContainer>
         )}
-        
       </div>
     </div>
   );
