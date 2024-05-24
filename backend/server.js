c925ec96356dd25e1a7734ee29dc5edfe21291f4
Silohi Andreea <andreea.silohi@bearingpoint.com>
2024-05-24 11:56:07 +0300

    added from tutorial

--- backend/server.js
+++ backend/server.js
@@ -40,9 +40,11 @@ app.use("/api/wishlist", wishlistRouter);
 app.use("/api/cart", cartRouter); // Use cart routes
 app.use("/api/email",emailRouter);
 
-app.get('/', function (req, res) {
-  res.render('index', {});
-});
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
 //this is like a middleware
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
